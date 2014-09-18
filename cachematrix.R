4c1b0f77ef5a169adea531bf374b9181fed7ee14
Rachel <rachel.jiling@gmail.com>
2014-09-18 20:08:28 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,16 @@
-## The role of this function
+## The role of this function is to creates a special "matrix" object that can cache its inverse.
 
 ## A function that makes functions
 
 makeCacheMatrix <- function(x = matrix()) {
-        s <- NULL               ## Define
+        s <- NULL               ## Define the value that stores inverse value
         set <- function(y) {
         x <- y
         s <<- NULL
         }
         get <- function() x ## Get value of x
         setsolve <- function(solve) s <<- solve
-        getsolve <- function() s
+        getsolve <- function() s  ## Get the inverse
         list(set = set, get = get,
         setsolve = setsolve,
         getsolve = getsolve)
@@ -21,4 +21,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getx()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$getx()
+        s <- solve(data, ...) #use solve to compute the inverse of a square matrix
+        x$setmean(s)
+        s
 }
+
